a1c724e98ffb3c4de738c50c9fc165b7829c2a8f
Lukas Elmer <lukas.elmer@gmail.com>
2017-08-06 12:29:14 +0200

    Implement aws upload

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"log"
@@ -8,6 +9,11 @@ import (
 	"os"
 	"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awsutil"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -37,33 +43,80 @@ func downloadFile(url string, destination string) (err error) {
 	return nil
 }
 
-func handleDownload(fileURL string) (string, bool) {
+func handleDownload(fileURL string) (gin.H, bool) {
 	arr := strings.Split(fileURL, "/")
 	filename := arr[len(arr)-1]
 
+	// the filename validation is very basic and possibly enables a security issues
 	if !strings.HasSuffix(filename, ".pdf") {
-		return fmt.Sprintf("invalid filename %s", filename), false
+		return gin.H{"error": "invalid filename " + filename}, false
 	}
 
 	destinationPath := fmt.Sprintf("tmp/%s", filename)
 	if downloadFile(fileURL, destinationPath) != nil {
-		return fmt.Sprintf("error downloading %s", fileURL), false
+		return gin.H{"error": "error downloading " + fileURL}, false
 	}
 
-	// os.Getenv("API_KEY")
+	uploadToS3(filename)
+
 	publicURL := fmt.Sprintf("%s/download/%s", os.Getenv("BASE_URL"), filename)
-	return string(publicURL), true
+
+	publicAwsURL := fmt.Sprintf("https://s3-%s.amazonaws.com/%s/files/%s",
+		os.Getenv("AWS_REGION"), os.Getenv("AWS_BUCKET"), filename)
+
+	return gin.H{"temporaryLink": publicURL, "persistentLink": publicAwsURL}, true
+}
+
+func uploadToS3(filename string) {
+	creds := credentials.NewEnvCredentials()
+	creds.Get()
+
+	_, err := creds.Get()
+	if err != nil {
+		fmt.Printf("bad credentials: %s", err)
+	}
+
+	cfg := aws.NewConfig().WithRegion(os.Getenv("AWS_REGION")).WithCredentials(creds)
+	svc := s3.New(session.New(), cfg)
+
+	file, err := os.Open("tmp/" + filename)
+	if err != nil {
+		fmt.Printf("err opening file: %s", err)
+	}
+	defer file.Close()
+
+	fileInfo, _ := file.Stat()
+	var size = fileInfo.Size()
+
+	buffer := make([]byte, size)
+	file.Read(buffer)
+	fileBytes := bytes.NewReader(buffer)
+	fileType := http.DetectContentType(buffer)
+
+	path := "files/" + filename
+	params := &s3.PutObjectInput{
+		Bucket:        aws.String(os.Getenv("AWS_BUCKET")),
+		Key:           aws.String(path),
+		Body:          fileBytes,
+		ContentLength: aws.Int64(size),
+		ContentType:   aws.String(fileType),
+	}
+	resp, err := svc.PutObject(params)
+	if err != nil {
+		fmt.Printf("bad response: %s", err)
+	}
+	fmt.Printf("response %s", awsutil.StringValue(resp))
 }
 
 func storeHandler(c *gin.Context) {
 	apiKey := os.Getenv("API_KEY")
 	apiKeyParam := c.Query("api_key")
 	if apiKey == apiKeyParam {
-		link, success := handleDownload(c.Query("url"))
+		response, success := handleDownload(c.Query("url"))
 		if success {
-			c.JSON(http.StatusOK, gin.H{"link": link})
+			c.JSON(http.StatusOK, response)
 		} else {
-			c.JSON(http.StatusOK, gin.H{"error": link})
+			c.JSON(http.StatusUnprocessableEntity, response)
 		}
 	} else {
 		c.String(http.StatusUnauthorized, fmt.Sprintf("invalid api key %s", apiKeyParam))
